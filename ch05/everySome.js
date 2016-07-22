f2c987ef1fa0a954fb0e6e9bfd5098ba4c1efca8
Zhuyi Xue <alfred532008@gmail.com>
2016-07-22 17:41:45 +0000

    written everySome.js

---
+++ ch05/everySome.js
@@ -0,0 +1,29 @@
+function every(array, func) {
+    for (var i=0; i<array.length; i++) {
+        if (!func(array[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+function some(array, func) {
+    for (var i=0; i<array.length; i++) {
+        if (func(array[i])) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+console.log(every([NaN, NaN, NaN], isNaN));
+// → true
+console.log(every([NaN, NaN, 4], isNaN));
+// → false
+console.log(some([NaN, 3, 4], isNaN));
+// → true
+console.log(some([2, 3, 4], isNaN));
+// → false
